6595d60691018372c9aaae57f53eb5f1c138dfdd
icefox0801 <icefox0801@hotmail.com>
2021-02-03 11:45:22 +0800

    fix(upper-bound): upperBound

--- LIS/lengthOfLIS.js
+++ LIS/lengthOfLIS.js
@@ -12,10 +12,12 @@ function lowerBound (nums, target) {
 function upperBound (nums, target) {
   let l = 0, r = nums.length - 1, mid;
 
-  while (mid = Math.floor((l + r) / 2), l < r) {
-    if (nums[mid] < target) r = mid;
-    else l = mid + 1;
+  while (mid = Math.ceil((l + r) / 2), l < r) {
+    if (nums[mid] < target) l = mid;
+    else r = mid - 1;
   }
+
+  return l;
 }
 
 function lengthOfLIS (nums) {
